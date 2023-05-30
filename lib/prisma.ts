55d52b073fbac522d69b2f424119326214664652
Shane Howe <shanehowe100@hotmail.com>
2023-05-30 17:34:27 +0100

    prisma singleton

---
+++ lib/prisma.ts
@@ -0,0 +1,13 @@
+// ts-ignore 7017 is used to ignore the error that the global object is not
+// defined in the global scope. This is because the global object is only
+// defined in the global scope in Node.js and not in the browser.
+
+import { PrismaClient } from '@prisma/client'
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
+
+export const prisma = globalForPrisma.prisma || new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+export default prisma
