7f70e2918a6871a12d2f8ffeca2e3399aec0e1cb
Shane Howe <shanehowe100@hotmail.com>
2023-06-03 21:38:21 +0100

    wrap async function bodies in try catch

--- app/api/exercises/route.ts
+++ app/api/exercises/route.ts
@@ -2,8 +2,12 @@ import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-    const exercises = await prisma.exercise.findMany({})
-    return NextResponse.json({
-        data: exercises
-    })
+    try {
+        const exercises = await prisma.exercise.findMany({})
+        return NextResponse.json({
+            data: exercises
+        })
+    } catch(e) {
+        return NextResponse.json({}, {status: 500})
+    }
 }
--- app/api/users/signup/route.ts
+++ app/api/users/signup/route.ts
@@ -3,51 +3,61 @@ import bcrypt from "bcrypt"
 import { NextResponse } from "next/server";
 
 type SignUpData = {
-    email: string
-    username: string
-    password: string
+    email?: string
+    username?: string
+    password?: string
 }
 
 export async function POST(request: Request) {
     const body: SignUpData = await request.json()
     const { email, username, password } = body
 
-    let existingUser = await prisma.user.findFirst({
-        where: { email }
-    })
-
-    if (existingUser) {
-        return NextResponse.json(
-            { message: "Email already in use" },
-            { status: 400 }
-        )
+    if (!email || !username || !password) {
+        return NextResponse.json({
+            error: "Missing values"
+        }, { status: 400 })
     }
 
-    existingUser = await prisma.user.findFirst({
-        where: { username }
-    })
-
-    if (existingUser) {
+    try {
+        let existingUser = await prisma.user.findFirst({
+            where: { email }
+        })
+    
+        if (existingUser) {
+            return NextResponse.json(
+                { message: "Email already in use" },
+                { status: 400 }
+            )
+        }
+    
+        existingUser = await prisma.user.findFirst({
+            where: { username }
+        })
+    
+        if (existingUser) {
+            return NextResponse.json(
+                { message: "Username already in use" },
+                { status: 400 }
+            )
+        }
+    
+        const SALT_ROUNDS = 12
+        const salt = await bcrypt.genSalt(SALT_ROUNDS)
+        const passwordHash = await bcrypt.hash(password, salt)
+    
+        const user = await prisma.user.create({
+            data: { username, email, passwordHash }
+        })
+    
+        const userWithoutPassword = exclude(user, ["passwordHash"])
+    
         return NextResponse.json(
-            { message: "Username already in use" },
-            { status: 400 }
+            { data: userWithoutPassword },
+            { status: 200 }
         )
+    } catch(e) {
+        return NextResponse.json({}, { status: 500 })
     }
-
-    const SALT_ROUNDS = 12
-    const salt = await bcrypt.genSalt(SALT_ROUNDS)
-    const passwordHash = await bcrypt.hash(password, salt)
-
-    const user = await prisma.user.create({
-        data: { username, email, passwordHash }
-    })
-
-    const userWithoutPassword = exclude(user, ["passwordHash"])
-
-    return NextResponse.json(
-        { data: userWithoutPassword },
-        { status: 200 }
-    )
 }
 
 function exclude<User, Key extends keyof User>(
