020e392b9e6fa4aa405874fd05eb16ee03a060b1
Shane Howe <shanehowe100@hotmail.com>
2023-06-04 22:31:30 +0100

    helper functions for workout folders

---
+++ lib/workoutFolders.ts
@@ -0,0 +1,21 @@
+const folderAlreadyExists = (folderName: string, currentFolders: ExerciseFolder[]): boolean => {
+    return currentFolders.filter(folder => folder.folderName === folderName).length > 0
+}
+
+const trimAndTitleFolderName = (folderName: string): string => {
+    const splitArr = folderName.split(" ")
+    let joined = ""
+    splitArr.forEach(word => {
+        if (word.length > 2) {
+            joined += word[0].toLocaleUpperCase() + word.substring(1).toLocaleLowerCase() + " "
+        } else {
+            joined += word.toLocaleLowerCase() + " "
+        }
+    })
+    return joined.trim()
+}
+
+export const workoutFolderHelper = {
+    folderAlreadyExists,
+    trimAndTitleFolderName
+}
