2d251bd2fd4cb2c47ba076ba42e7d579a545b77f
Shane Howe <shanehowe100@hotmail.com>
2023-06-05 14:16:42 +0100

    turn page to client component until seperate components have been built

--- app/workout-folders/[folder]/page.tsx
+++ app/workout-folders/[folder]/page.tsx
@@ -1,3 +1,8 @@
+"use client"
+import { workoutFolderService } from "@/services/workoutFolders"
+import { Button } from "@chakra-ui/react"
+import { useRouter } from "next/navigation"
+
 interface PageProps {
     params: {
         folder: string
@@ -5,11 +10,26 @@ interface PageProps {
 }
 
 export default function Page({ params }: PageProps) {
+    const router = useRouter()
     /*
     *   decodeURI turns Push%20Day to Push Day
     */
     const folder = decodeURIComponent(params.folder)
+    const handleDelete = async (id: string) => {
+        const folderId = parseInt(folder)
+        try {
+            const res = await workoutFolderService.deleteFolder(folderId)
+            if (res.status === 200) {
+                router.push("/workout-folders")
+            }
+        } catch(e) {
+            console.error(e)
+        }
+    }
     return (
+        <>
         <p>From {folder}</p>
+        {folder === "All Exercises" ? "" : <Button onClick={() => handleDelete(folder)}>Delete Folder</Button>}
+        </>
     )
 }
