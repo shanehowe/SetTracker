aa1c434fe44eae45cd0c9f7be2afb24503cfa20e
Shane Howe <shanehowe100@hotmail.com>
2023-06-05 23:13:22 +0100

    fetch folder exercises through use effect. render folder name

--- app/workout-folders/[folder]/page.tsx
+++ app/workout-folders/[folder]/page.tsx
@@ -1,7 +1,11 @@
 "use client"
+import LoadingSpinner from "@/components/spinner/LoadingSpinner"
 import { workoutFolderService } from "@/services/workoutFolders"
-import { Button } from "@chakra-ui/react"
+import { Button, Flex, Icon, Text } from "@chakra-ui/react"
+import { FolderExercise } from "@prisma/client"
 import { useRouter } from "next/navigation"
+import { useEffect, useState } from "react"
+import { FiFolder } from "react-icons/fi"
 
 interface PageProps {
     params: {
@@ -10,11 +14,30 @@ interface PageProps {
 }
 
 export default function Page({ params }: PageProps) {
+    const [folderExercises, setFolderExercises] = useState<FolderExercise[] | null>(null)
+    const [folderName, setFolderName] = useState("")
+
     const router = useRouter()
-    /*
-    *   decodeURI turns Push%20Day to Push Day
-    */
     const folder = decodeURIComponent(params.folder)
+
+    useEffect(() => {
+        const maybeFolderId = parseInt(folder)
+        if (!isNaN(maybeFolderId)) {
+            workoutFolderService
+                .getOne(maybeFolderId)
+                .then(res => res.json())
+                .then(res => {
+                    console.log(res)
+                    setFolderExercises(res.data.folderExercises)
+                    setFolderName(res.data.folderName)
+                })
+                .catch(err => console.error(err))
+        } else {
+            setFolderExercises([])
+            setFolderName("All Exercies")
+        }
+    },[folder])
+
     const handleDelete = async (id: string) => {
         const folderId = parseInt(folder)
         try {
@@ -26,10 +49,18 @@ export default function Page({ params }: PageProps) {
             console.error(e)
         }
     }
-    return (
-        <>
-            <p>From {folder}</p>
-            {folder === "All Exercises" ? "" : <Button onClick={() => handleDelete(folder)}>Delete Folder</Button>}
-        </>
+    return folderExercises === null ? (
+        <LoadingSpinner />
+    ) : (
+        <section>
+            <Flex w={"100%"} direction={"column"} alignItems={"center"} mt={12}>
+                <Flex dir="row" alignItems={"center"}>
+                    <Icon as={FiFolder} mr={3} w={21} h={21}/>
+                    <div>
+                        <Text fontSize={22}>{folderName}</Text>
+                    </div>
+                </Flex>
+            </Flex>
+        </section>
     )
 }
