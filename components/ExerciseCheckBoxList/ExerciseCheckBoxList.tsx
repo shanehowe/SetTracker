f49a49c165ac31f2a86ce96a65270dea09722bb8
Shane Howe <shanehowe100@hotmail.com>
2023-06-09 21:46:39 +0100

    Move out into component, allows to be reused. Obvious decision

---
+++ components/ExerciseCheckBoxList/ExerciseCheckBoxList.tsx
@@ -0,0 +1,26 @@
+import { CheckboxGroup, Stack, Checkbox } from "@chakra-ui/react"
+
+interface ExerciseCheckboxListProps {
+    exercises: Exercise[]
+    handleExerciseClick: (exercise: string) => void
+}
+
+export function ExerciseCheckboxList({ exercises, handleExerciseClick }: ExerciseCheckboxListProps) {
+    return (
+        <CheckboxGroup>
+            <Stack spacing={[1, 5]} direction={"column"}>
+                {exercises.map((exercise) => {
+                    return (
+                        <Checkbox
+                            value={exercise.name}
+                            key={exercise.id}
+                            onChange={() => handleExerciseClick(exercise.name)}
+                        >
+                            {exercise.name}
+                        </Checkbox>
+                    )
+                })}
+            </Stack>
+        </CheckboxGroup>
+    )
+}
