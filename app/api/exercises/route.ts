6800a48863e3bed5ff8aab8f11ce63a5a1536ded
Shane Howe <shanehowe100@hotmail.com>
2023-06-09 21:41:58 +0100

    remove request argument its not needed

--- app/api/exercises/route.ts
+++ app/api/exercises/route.ts
@@ -1,7 +1,7 @@
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request) {
+export async function GET() {
     try {
         const exercises = await prisma.exercise.findMany({})
         return NextResponse.json({
