2af254d1bab3c203001e57b80035246cdbb5ad9a
Shane Howe <shanehowe100@hotmail.com>
2023-06-17 13:47:42 +0100

    new sets service module, unfinished

---
+++ services/sets.ts
@@ -0,0 +1,9 @@
+/*
+* This module will be responsible
+* for retrieving and updating sets
+* belonging to a specific exercise.
+*/
+
+const getAll = (exerciseName: string) => {
+    throw new Error("Not implemented yet")
+}
