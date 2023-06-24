fb51e700241f7ff1904aec0bf27858f5d897e405
Shane Howe <shanehowe100@hotmail.com>
2023-06-24 16:08:26 +0100

    function for sorting set group by date

---
+++ lib/sets.ts
@@ -0,0 +1,14 @@
+export const sortGroupedSetsByDate = (sets: GroupedSet[]) => {
+    sets.sort((a: GroupedSet, b: GroupedSet) => {
+        const dateA = new Date(a.date);
+        const dateB = new Date(b.date);
+        
+        if (dateA < dateB) {
+            return 1;
+        } else if (dateA > dateB) {
+            return -1;
+        } else {
+            return 0;
+        }
+    })
+}
